5b3b39f05a1e0527c1c87b33ea58d8ed32730c3c
原点 <yuandian@brmyx.com>
2023-09-27 10:33:46 +0800

    complete ipfs ntf project

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+require("dotenv").config({path: ".env"});
+
+async function main() {
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5";
+  /*
+  DeployContract in ethers.js is an abstraction used to deploy new smart contracts,
+  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
+  */
+  // here we deploy the contract
+ const lw3PunksContract = await hre.ethers.deployContract("LW3Punks", [
+   metadataURL
+ ]);
+
+  await lw3PunksContract.waitForDeployment();
+
+ // print the address of the deployed contract
+  console.log("LW3Punks Contract Address:", lw3PunksContract.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
